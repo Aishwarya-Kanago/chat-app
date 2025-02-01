1f774e34dce1780e2baa8d71f45bb968b042aadf
Aishwarya-Kanago <aishwaryakanago21@gmail.com>
2025-02-01 15:02:10 +0530

    add creds true to cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 var corsOptions = {
   origin: ["http://localhost:3000"],
+  credentials: true,
   optionsSuccessStatus: 200,
   methods: "GET, PUT, POST, PATCH, DELETE",
 };
