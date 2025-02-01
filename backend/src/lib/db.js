570a8c60de6bff978142d45dd07c6cc1b12038b3
Aishwarya-Kanago <aishwaryakanago21@gmail.com>
2025-02-01 15:15:44 +0530

    connectDB family 4

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI, { family: 4 });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error: ", error);
